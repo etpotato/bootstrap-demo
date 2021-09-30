56ced2e4a44ee2b749ec73c01e79faf3e517d638
etpotato <mareev.pv@gmail.com>
2021-09-30 22:55:44 +0300

    Add dropdowns & lead section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,32 @@
 import 'bootstrap/js/dist/collapse';
 
-// console.log(bootstrap);
-console.log('Hello world');
+const setSelectlist = selectlist => {
+  const ACTIVE_CLASS = 'selectlist-active';
+  const TAB_CLASS = 'js-selectlist-choice';
+  const parent = selectlist;
+  const toggle = parent.querySelector('.js-selectlist-toggle');
+  const toggleImg = toggle.querySelector('.js-selectlist-toggle-img');
+  const tablist = parent.querySelector('.js-selectlist-tablist');
+  let isExpanded;
+
+  const toggleMenu = evt => {
+    evt.preventDefault();
+    toggle.classList.toggle(ACTIVE_CLASS);
+    isExpanded = toggle['aria-expanded'] === 'true';
+    toggle['aria-expanded'] = !isExpanded;
+    isExpanded = !isExpanded;
+  };
+
+  const handleTabClick = evt => {
+    evt.preventDefault();
+    if (!evt.target.classList.contains(TAB_CLASS)) return;
+    toggleImg.src = evt.target.querySelector('img').src;
+    toggleMenu(evt);
+  };
+
+  toggle.addEventListener('click', toggleMenu);
+  tablist.addEventListener('click', handleTabClick);
+};
+
+const selectlists = [...document.querySelectorAll('.js-selectlist')];
+if (selectlists.length) selectlists.forEach(selectlist => setSelectlist(selectlist));
